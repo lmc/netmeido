e3e6d6131e29a22a1729c32a495c1c4de30c05d0
Luke Mcildoon <luke@twofiftyfive.net>
2010-12-20 17:21:28 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cal2::Application.config.secret_token = '8cc02cd2004ef29f268aafa683584ed3c91eb58112da1977db591e360db134bd33266111f06b27f8534161162df3e2886aba8090c4da053ecb6fd45196962fac'
