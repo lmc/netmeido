2e217a4567a8bb42f575f5161f580bb37606074f
Luke Mcildoon <luke@twofiftyfive.net>
2011-03-06 22:12:16 +1100

    more specs!

--- spec/models/time_capsule_spec.rb
+++ spec/models/time_capsule_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe TimeCapsule do
+  
   describe "Success/Failure states" do
     
     it "should set success properly on success" do
@@ -31,5 +32,31 @@ describe TimeCapsule do
     end
     
   end
+  
+  describe "Parsing data" do
+    before(:each) do
+      TimeCapsule.should_receive(:snmpwalk).with("192.168.0.2").
+        and_return(File.read(Rails.root.join('spec','fixtures','snmpwalk_time_capsule.txt')))
+      @time_capsule = TimeCapsule.new("192.168.0.2")
+      @time_capsule.fetch!
+      @time_capsule.parse!
+      @data = @time_capsule.interface_stats
+    end
     
+    it "should parse the expected amount of interfaces" do
+      @data.should have(8).keys
+    end
+    
+    it "should parse data for the first interface correctly" do
+      @data["1"][:name].should        == "mgi0"
+      @data["1"][:type].should        == "ethernetCsmacd(6)"
+      @data["1"][:mac].should         == "0:26:bb:6c:3f:83"
+      @data["1"][:in].should          == 689150226
+      @data["1"][:out].should         == 165828714
+      @data["1"][:errors_in].should   == 0
+      @data["1"][:errors_out].should  == 0
+      @data["1"][:up].should          == true
+    end
+  end
+  
 end
